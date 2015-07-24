8bc7f5d86e005a8d30d0b643d487b09b1c91e41a
Anders Bolin <anders@bolin.nu>
2015-07-24 09:23:34 +0200

    upload project

---
+++ AgeevidencerTest/DAL.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace AgeevidencerTest
+{
+    public class DAL
+    {
+
+        //Variables
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DAL()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "aedbtest";
+            uid = "root";
+            password = "123123123.123123123";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public string getFirstName()
+        {
+            string xml = "<USER>";
+            if (OpenConnection())
+            {
+                string query = "SELECT * FROM user";
+
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Read results
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    xml += "<ID>" + dataReader["ID"] + "</ID>";
+                    xml += "<NAME>" + dataReader["Name"] + "</NAME>";
+                    xml += "<AGEMONTH>" + dataReader["AgeMonth"] + "</AGEMONTH>";
+                    xml += "<GENDER>" + dataReader["Gender"] + "</GENDER>";
+                    xml += "<WORLD>" + dataReader["World"] + "</WORLD>";
+                    xml += "<ACTIVE>" + dataReader["Active"] + "</ACTIVE>";
+                    xml += "<EMAIL>" + dataReader["Email"] + "</EMAIL>";
+                    xml += "<SCORE>" + dataReader["Score"] + "</SCORE>";
+                    xml += "<AGEYEAR>" + dataReader["AgeYear"] + "</AGEYEAR>";
+                    xml += "<PASSWORD>" + dataReader["Password"] + "</PASSWORD>";
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close connection
+                this.CloseConnection();
+
+                xml += "</USER>";
+
+                return xml;
+            }
+            else
+            {
+                xml = "DENIED</USER>";
+                return xml;
+            }
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                
+                return false;
+            }
+        }
+
+        public void InsertUser(string instr, string inwhere)
+        {
+            string query = "INSERT INTO user ("+inwhere+") VALUES("+instr+")";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ AgeevidencerTest/bin/WebService.asmx.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+using System.Xml;
+
+namespace AgeevidencerTest
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://www.ageevidencer.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod(Description = "Returns string if connection established")]
+        public string HelloWorld()
+        {
+            return "Connection established";
+        }
+
+       
+
+        [WebMethod(Description = "Returns string")]
+        public string getUserName()
+        {
+            //var json = "";
+            DAL dal = new DAL();
+            //dynamic result = dal.getFirstName();
+            //JavaScriptSerializer  jss = new JavaScriptSerializer();
+            //var jsonString = JsonConvert.SerializeObject(result);
+            //json = jss.Serialize(result);
+            string xml = dal.getFirstName();
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+            string json = Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc);
+
+            return json;
+        }
+
+        [WebMethod]
+        public string testXML()
+        {
+            string xml = "<Test><Name>Test class</Name><X>100</X><Y>200</Y></Test>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+            string json = Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc);
+            return json;
+        }
+
+    }
+}
